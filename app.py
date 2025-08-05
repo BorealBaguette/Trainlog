8bdefb3647d529d53defc09752f2b49308520479
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-08-05 22:06:44 +0200

    Add title to `/live_map`

--- app.py
+++ app.py
@@ -8950,6 +8950,7 @@ def live_map():
     """
     return render_template(
         "public/current_global.html",
+        title=lang[session["userinfo"]["lang"]]["live_map"],
         username=getUser(),
         logosList=listOperatorsLogos(),
         translations=lang[session["userinfo"]["lang"]],
