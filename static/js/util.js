1d771bc0e673193467bd6e890f0cd9b76bad04b8
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-07-26 16:16:55 +0200

    Add autocompletion for material type in mass edit

--- static/js/util.js
+++ static/js/util.js
@@ -885,15 +885,17 @@ function operatorAutocomplete(select, manAndOps, logos_url, no_logo_url) {
         response(sortedOperatorNames);
     },
     select: function (event, ui) {
-        var selectedOperator = ui.item.value;
-        var logoUrl = manAndOps.operators[selectedOperator] 
-                      ? logos_url + manAndOps.operators[selectedOperator] 
-                      : no_logo_url;
+        if (logos_url && no_logo_url) {
+          var selectedOperator = ui.item.value;
+          var logoUrl = manAndOps.operators[selectedOperator]
+                        ? logos_url + manAndOps.operators[selectedOperator]
+                        : no_logo_url;
 
-        $.ajax(logoUrl)
-            .done(function () {
-                $(".operatorLogo").attr("src", logoUrl);
-            })
+          $.ajax(logoUrl)
+              .done(function () {
+                  $(".operatorLogo").attr("src", logoUrl);
+              })
+        }
     }
   });
 }
